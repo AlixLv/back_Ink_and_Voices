e0cf1dd8be1a957315262e8fc931ceee252eccde
Térence Da Conceiçao <terence.daco@gmail.com>
2025-12-19 16:05:49 +0100

    chore: add supabase connected to fastify

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config'
 import Fastify from 'fastify';
+import { createClient } from '@supabase/supabase-js';
 
 const fastify = Fastify();
 
 const { ADDRESS = '0.0.0.0', PORT = '8000' } = process.env;
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('SUPABASE_URL and SUPABASE_KEY must be defined');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 fastify.get('/', async (request, reply) => {
   return { message: 'Hello world!' };
@@ -14,4 +25,7 @@ fastify.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
+  console.log(supabaseUrl);
 });
+
+export default supabase

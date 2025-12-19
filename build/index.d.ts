9356141d5aa618af2dcbe7c82ab8fff119ea7a06
Térence Da Conceiçao <terence.daco@gmail.com>
2025-12-19 16:05:49 +0100

    chore: add supabase connected to fastify

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,4 @@
-export {};
+import 'dotenv/config';
+declare const supabase: import("@supabase/supabase-js").SupabaseClient<any, "public", "public", any, any>;
+export default supabase;
 //# sourceMappingURL=index.d.ts.map

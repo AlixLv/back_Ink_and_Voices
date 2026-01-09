7a4d0f94af2a64a0e3f6e3360e80d357faba3966
Térence Da Conceiçao <terence.daco@gmail.com>
2026-01-09 16:05:12 +0100

    chore: hid build from git

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-
 import Fastify from 'fastify';
-//import supabase from "./../config/supabase.config.js";
 import { supabaseUrl } from "./config/supabase.config.js";
-//import supabase from 'C:/Users/Térence/Desktop/RNCP6-CODE/back_Ink_and_Voices/config/supabase.config'
 
 const fastify = Fastify();
-
 const { ADDRESS = '0.0.0.0', PORT = '8000' } = process.env;
-
-
 
 fastify.get('/', async (request, reply) => {
   return { message: 'Hello world!' };

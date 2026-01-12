40cbb46b33a6fa3d2f652772308859dbaf3e6b48
Térence Da Conceiçao <terence.daco@gmail.com>
2026-01-12 11:20:39 +0100

    chore: cleaned supabase.config.ts code
    refacto

--- src/config/supabase.config.ts
+++ src/config/supabase.config.ts
@@ -1,14 +1,9 @@
 import 'dotenv/config';
-
 import { createClient } from '@supabase/supabase-js';
-
 export const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
 
+const supabaseKey = process.env.SUPABASE_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('SUPABASE_URL and SUPABASE_KEY must be defined');
 }
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-// export default supabase
+export const supabase = createClient(supabaseUrl, supabaseKey);

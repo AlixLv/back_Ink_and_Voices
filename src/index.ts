3426f0f33c8e40282f246de997825867735bfa4d
Alix <alix.leve@gmail.com>
2026-01-13 14:35:40 +0100

    chore:add prisma connexion to supabase

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ fastify.get('/', async (request, reply) => {
 
 fastify.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
   if (err) {
-    console.error(err);
+    console.error("🚨 ",err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);

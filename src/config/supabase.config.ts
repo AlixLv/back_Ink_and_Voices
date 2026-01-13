8f62a77da8f0ee1ddeab2ee221fd041b778d4f7f
Alix <alix.leve@gmail.com>
2026-01-13 10:05:10 +0100

    chore: change command npm run dev to npm start in docker-compose

--- src/config/supabase.config.ts
+++ src/config/supabase.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 export const supabaseUrl = process.env.SUPABASE_URL;
 
-const supabaseKey = process.env.SUPABASE_KEY;
+export const supabaseKey = process.env.SUPABASE_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('SUPABASE_URL and SUPABASE_KEY must be defined');
 }
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);

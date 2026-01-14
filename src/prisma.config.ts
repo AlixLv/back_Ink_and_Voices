fe35070d0ec9ec0cc5e3d2afb56b43bc7c47ce99
Térence Da Conceiçao <terence.daco@gmail.com>
2026-01-14 15:53:52 +0100

    chore: node prisma types problem is fixed

--- src/prisma.config.ts
+++ src/prisma.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["DATABASE_URL"] || "",
   },
 });
 console.log("🌼", process.env["DATABASE_URL"]);

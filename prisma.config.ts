42d28a8ccafcdcdab8d0cc4168a036556c37c0be
Térence Da Conceiçao <terence.daco@gmail.com>
2026-01-30 11:38:09 +0100

    chore: fixed node types problem in prisma and ts config

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,18 +7,20 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
-const databaseURL = isDevelopment
-  ? process.env["LOCAL_DATABASE_URL"]
-  : process.env["DATABASE_URL"];
+const databaseURL = isDevelopment ? process.env["LOCAL_DATABASE_URL"] : process.env["DATABASE_URL"];
+
+if (!databaseURL) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts"
-  },
+ },
   datasource: {
-    url: process.env["DATABASE_URL"] || "",
+    url: databaseURL,
   },
 });
 console.log("🌼", process.env["DATABASE_URL"]);

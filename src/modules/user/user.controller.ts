327d5d1a8d94f88bf42e58796b3ce4835da0692a
Alix <alix.leve@gmail.com>
2026-02-27 16:39:32 +0100

    feature authenticate: installed npm install argon2 @types/argon2 and npm install @node-rs/argon2 for hash password

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import type { CreateUserInput } from './user.schema.js';
+import * as argon2 from 'argon2';
 
-const SALT_ROUNDS = 10
 
 export async function registerUserHandler(
     req: FastifyRequest<{Body: CreateUserInput}>,
@@ -9,12 +9,24 @@ export async function registerUserHandler(
 ){
     const { email, username, password } = req.body;
 
+    const isUser = await req.server.prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    })
+    if (isUser) {
+        return reply.code(409).send({
+            message: 'A user already exists with this email.',
+        })
+    }
+
     try {
+        const hashPassword = await argon2.hash(password);
         const user = await req.server.prisma.user.create({
             data: {
                 email,
                 username,
-                password,
+                password: hashPassword,
             }
         })
         return reply.code(201).send(user)
@@ -22,3 +34,4 @@ export async function registerUserHandler(
         return reply.code(500).send(e)
     }
 }
+

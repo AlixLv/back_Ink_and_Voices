13ac3425dee283b82df55f21aaf97aa24c1508c1
Térence Da Conceiçao <terence.daco@gmail.com>
2026-03-06 11:02:17 +0100

    feat: connexion finale à prismaClient en se basant sur le code d'alix pour register modif dans dockerfile et docker-compose : args DATABASE_URL, rajout de Argon, création de user.schema et user.controller, changement dans l'output pour geenrated afin qu'il se place dans src, rajout de fastify.d.ts afin de lancer le prismaClient à chaque instance du serveur etc etc

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -2,7 +2,11 @@ import type { FastifyInstance } from 'fastify'
 import { z } from 'zod';
 import {type ZodTypeProvider } from 'fastify-type-provider-zod';
 import { createUserSchema, createUserResponseSchema } from './user.schema.js';
+<<<<<<< HEAD
 import { signUp } from './user.controller.js';
+=======
+import { registerUserHandler } from './user.controller.js';
+>>>>>>> b2190da (feat: connexion finale à prismaClient en se basant sur le code d'alix pour register)
 
 
 export async function userRoutes(app: FastifyInstance) {

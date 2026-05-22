7b1547095565e4608fa56e705dad79442ea833f1
Térence Da Conceiçao <terence.daco@gmail.com>
2026-05-22 15:15:54 +0200

    feat: commented ignoreDeprecations, add correct config to vitestconfig dotenv

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vitest/config'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   test: {
     // options de configuration de vitest
     environment: 'node',
     globals: true, // pour utiliser les fonctions expect, describe, etc. sans les importer
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    //include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], // pour inclure les fichiers de test dans le dossier srcet pas dans build afin de ne pas lancer les tests compilés.
   },
 })

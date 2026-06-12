247b56fba3656c3884aa21f440df15e5fc0080bc
Térence Da Conceiçao <terence.daco@gmail.com>
2026-06-12 15:36:09 +0200

    syntaxe describe() sur tous tests. Refacto. Suppression des tests inutiles.

--- src/tests/auth.controller.test.ts
+++ src/tests/auth.controller.test.ts
@@ -1,38 +1,14 @@
-import { expect, expectTypeOf, test } from 'vitest'
+import { expect, it, describe } from 'vitest';
 import { createUserSchema } from '../modules/auth/auth.schema'
-import { signUp } from '../modules/auth/auth.controller'
 import 'dotenv/config'
 
- // TODO: Vérification des messages d'erreur et de succès!
- // TODO: que doit-on vérifier dans ces tests?
- // TODO: respecter la convention describe/it
-
-
-// Vérification des types TypeScript
-test('my types work properly', () => {
-    expectTypeOf(signUp).toBeFunction()
-    expectTypeOf({email: "ada@gmail.com"}).toEqualTypeOf<{email: string}>()
-    expectTypeOf({username: "Ada"}).toEqualTypeOf<{username: string}>()
-    expectTypeOf({password: "testpwd"}).toEqualTypeOf<{password: string}>()
-  })
-
-  
-test('email valide', () => {
-    const result = createUserSchema.safeParse({
-        email: "ada@gmail.com",
-        username: "Ada",
-        password: "testpassword123"
-    })
-    console.log("result.error: ", result.error?.flatten())
-    expect(result.success).toBe(true)
-})
-
-
-test('email invalide', () => {
-    const result = createUserSchema.safeParse({
-        email: "not-an-email",
-        username: "Ada",
-        password: "testpassword123"
-    })
-    expect(result.success).toBe(false)
-})
+describe('cannot login with an invalid email', () => {
+    it('returns False', () => { // quelle status d'erreur? voir avec postman
+        const result = createUserSchema.safeParse({ 
+            email: "not-a-email",
+            username: "Ada",
+            password: "testpassword123"
+        })
+    expect(result.success).toBe(false);
+    });
+});

c229fd895bb2513e3cdcc811a64f4267fbc8d266
Alix <alix.leve@gmail.com>
2026-06-12 11:09:53 +0200

    moved tests files into auth and user modules; add test for user routes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vitest/config'
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-  test: {
-    // options de configuration de vitest
-    environment: 'node',
-    env: {
-      NODE_ENV: 'test'
-    },
-    globals: true, // pour utiliser les fonctions expect, describe, etc. sans les importer
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+export default defineConfig(({mode}) => {
+  // chargement du fichier .env pour avoir accès aux variables
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return { 
+    test: {
+      env: {
+        LOCAL_DATABASE_URL: process.env.LOCAL_DATABASE_URL
+      },
+      environment: 'node',
+      globals: true, // pour utiliser les fonctions expect, describe, etc. sans les importer
+      include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    }
+  }
 })
+
